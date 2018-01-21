c0b5325b0e09813da65424efc3e38ea6552e83b9
James Harper <jameswilliamharper@gmail.com>
2018-01-21 18:45:11 +0000

    Don't replace urls in img tags

--- src/helpers.js
+++ src/helpers.js
@@ -24,8 +24,8 @@ export const emojify = (text, height = 26) => {
  * @param {boolean} openExternal Determine if the link should open in a new tab
  * @returns {string} The url wrapped in an <a> tag
  */
-export const linkify = (text, openExternal = false) => text.replace(
-  /((http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)/g,
+export const linkify = (text) => text.replace(
+  /(?<!src=[\'"])(((f|ht){1}tps?:\/\/)[-a-zA-Z0-9@:%_\+.~#?&\/\/=]+)/i,
   `<a href="$1" ${openExternal ? 'target="_blank"' : ''}>$1</a>`
 );
 
